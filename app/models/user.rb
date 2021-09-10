98825b074febaddaebfa7464bf3e6e0e7dbb94c0
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-09-10 10:58:23 -0700

    Coding Challange phase 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :reviews
+  has_many :products, through: :reviews
+
+  def favorite_product 
+    Product.all.sort_by {|product| product.star_rating}.last
+  end 
+
+  def remove_reviews(product) 
+    review = self.reviews.find_by_product_id(product.id)
+    review.destroy if product
+  end
     
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 puts "Deleting old data..."
 Product.destroy_all
 User.destroy_all
+Review.destroy_all
 
 puts "Creating users..."
 user1 = User.create(name: Faker::Name.name)
@@ -22,5 +23,42 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
+review1 = Review.create(star_rating: 1, comment: "Hi", user_id: user1.id, product_id: product1.id)
+review2 = Review.create(star_rating: 2, comment: "Hello", user_id: user2.id, product_id: product2.id)
+review3 = Review.create(star_rating: 3, comment: "How are you", user_id: user3.id, product_id: product3.id)
 
 puts "Seeding done!"
+puts 
+puts "Review#user for user1"
+puts review1.user.name
+puts 
+puts "Review#product for user1"
+puts review2.product.price
+puts 
+puts "Product reviews and users"
+puts product1.reviews.map(&:comment)
+puts 
+puts product1.users.map(&:name)
+puts 
+puts "User reviews and product"
+puts user1.reviews.map(&:star_rating)
+puts 
+puts user1.products.map(&:name)
+puts 
+puts "Print review for review3"
+puts review3.print_review
+puts 
+puts "Testting leave review method"
+# puts stapler_review.print_review
+
+puts "removing review1"
+puts user1.reviews
+puts user2.remove_reviews(product2)
+puts "High start rating"
+puts product1.favorite_product.name
+puts "favorite_product"
+puts user1.favorite_product
+
+
+
+puts
